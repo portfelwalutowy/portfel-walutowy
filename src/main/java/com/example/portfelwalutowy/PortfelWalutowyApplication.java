a10e9b12c1ce1e8b7972915e697a12a7e246bd29
MaBi0110 <biziewskamk@gmail.com>
2023-08-08 22:49:06 +0200

    initial commit

---
+++ src/main/java/com/example/portfelwalutowy/PortfelWalutowyApplication.java
@@ -0,0 +1,13 @@
+package com.example.portfelwalutowy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfelWalutowyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortfelWalutowyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/portfelwalutowy/PortfelWalutowyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.portfelwalutowy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfelWalutowyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
